e60abf5c38b7294aaddaa0a000e1b5f125e08847
Andrea Difino <adifino13@gmail.com>
2026-01-08 21:08:30 +0100

    Sistemato altro tipo

--- backend/utils/device.py
+++ backend/utils/device.py
@@ -7,7 +7,7 @@ class Device():
     device_name : str 
     device_assets : List[dict[str, str]]
 
-    def __init__(self, device_name: str, device_assets: List[object]):
+    def __init__(self, device_name: str, device_assets: List[dict[str, str]]):
         self.device_name = device_name
         self.device_assets = device_assets
 
